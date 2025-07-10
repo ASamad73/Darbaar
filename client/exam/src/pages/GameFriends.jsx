5827cb2b8d089b52a4a5c0847e8bdef00a20cc2e
Abdul Samad <junaid.abdulsamad@gmail.com>
2025-07-10 21:37:24 +0500

    friends

--- client/exam/src/pages/GameFriends.jsx
+++ client/exam/src/pages/GameFriends.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState, useRef } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
 import '../../Design/css/game.css';
-import socket from '../../socket.js'
+import { createSocket } from '../../socket.js';
 
 export default function GameFriends() {
     const navigate = useNavigate();
@@ -19,8 +19,11 @@ export default function GameFriends() {
     const [timeout, setTimeout]=useState(false);
     const hasJoinedRef = useRef(false); 
     const gameRoleRef = useRef(null);
+    const socket = useRef(null);
 
     useEffect(() => {
+        socket.current = createSocket();
+
         const fetchData = async () => {
             const curr_username= localStorage.getItem('username');
 
@@ -45,14 +48,14 @@ export default function GameFriends() {
             setUserData(me);
 
             if (!hasJoinedRef.current) {
-                socket.emit('joining_friend', me);
+                socket.current.emit('joining_friend', me);
                 hasJoinedRef.current = true; 
             }
         };
         
         fetchData();
         
-        socket.on('start_game', (role, other_usernames)=>{
+        socket.current.on('start_game', (role, other_usernames)=>{
             setStart(true);
             console.log('role set: ', role);
             setGameRole(role);
@@ -60,26 +63,26 @@ export default function GameFriends() {
             setPlayersUsernames(other_usernames);
         });
 
-        socket.on('votes', (votes)=>{
+        socket.current.on('votes', (votes)=>{
             setAllVotes(votes);
         })
 
-        socket.on('guessed', ()=>{
+        socket.current.on('guessed', ()=>{
             console.log('guessed received');
             setGuess(true)
         })
 
-        socket.on('reveal_roles', (records)=>{
+        socket.current.on('reveal_roles', (records)=>{
             setRoleRecords(records);
         })
 
-        socket.on('msg_info', (info)=>{
+        socket.current.on('msg_info', (info)=>{
             console.log('message recived by', gameRoleRef.current);
             console.log('message recieved', info);
             setAllMessages(prev=>[...prev, info]);
         })
         
-        socket.on('end', (records)=>{
+        socket.current.on('end', (records)=>{
             console.log('end received by:', gameRoleRef.current); // now logs correct role
             setRoleRecords(records);
             if (gameRoleRef.current === 'Chor') {
@@ -89,30 +92,30 @@ export default function GameFriends() {
             }
         })
 
-        socket.on('won', ()=>{
+        socket.current.on('won', ()=>{
             setResult('You Won');
         })
 
-        socket.on('lost', ()=>{
+        socket.current.on('lost', ()=>{
             setResult('You Lost');
         })
         
 
-        socket.on('timeout', (msg)=>{
+        socket.current.on('timeout', (msg)=>{
             setResult(msg);
             setTimeout(true);
         })
 
         return()=>{
-            socket.off('start_game');
-            socket.off('votes');
-            socket.off('guessed');
-            socket.off('reveal_roles');
-            socket.off('msg_info');
-            socket.off('end');
-            socket.off('won');
-            socket.off('lost');
-            socket.off('timeout');
+            socket.current.off('start_game');
+            socket.current.off('votes');
+            socket.current.off('guessed');
+            socket.current.off('reveal_roles');
+            socket.current.off('msg_info');
+            socket.current.off('end');
+            socket.current.off('won');
+            socket.current.off('lost');
+            socket.current.off('timeout');
         }
 
     }, []);
@@ -122,10 +125,10 @@ export default function GameFriends() {
             console.log('voted for: ', vote);
             if(guess){
                 console.log('final voting');
-                socket.emit('final_vote', vote);
+                socket.current.emit('final_vote', vote);
             }
             else{
-                socket.emit('vote', userData.username, vote);
+                socket.current.emit('vote', userData.username, vote);
             }
         }
     }, [vote])
@@ -139,21 +142,21 @@ export default function GameFriends() {
 
     const handleGuess=()=>{
         if(start && result==='In Game'){
-            socket.emit('guess')
+            socket.current.emit('guess')
             setGuess(true);
         }
     }
 
     const handleEndGame=()=>{
         if(start){
-            socket.emit('end');
+            socket.current.emit('end');
         }
     }
 
     const sendMessage=()=>{
         console.log('message: ', message);
         setMessage('');
-        socket.emit('message', [[userData.username, gameRole], message])
+        socket.current.emit('message', [[userData.username, gameRole], message])
     }
 
     const handleReturn=async ()=>{
